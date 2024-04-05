f2deae3ff89a3ac89c67f7615e643f6df059605e
Julia Koch <koch.julia.1998@outlook.de>
2024-04-05 09:01:24 +0000

    task completed

--- RPS.py
+++ RPS.py
@@ -1,10 +1,22 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_pl, opp_hist=[], pl_ord={}):
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if not prev_pl:
+        prev_pl = 'S'
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    opp_hist.append(prev_pl)
+    pred = 'R'
 
-    return guess
+    if len(opp_hist)>4:
+        last = "".join(opp_hist[-5:])
+        pl_ord[last] = pl_ord.get(last, 0) + 1
+        
+        pot_pl = ["".join([*opp_hist[-4:], v]) for v in ['R', 'P', 'S']]
+
+        sub_ord = {k: pl_ord[k] for k in pot_pl if k in pl_ord}
+
+        if sub_ord:
+            pred = max(sub_ord, key=sub_ord.get)[-1:]
+
+    ideal_res = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    return ideal_res[pred]
